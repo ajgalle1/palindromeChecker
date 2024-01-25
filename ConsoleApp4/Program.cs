8b8c0b3ee19f90c10c80d252aed90b5ce0501a40
Andrew <andrew.j.galle@outlook.com>
2024-01-25 10:12:21 -0500

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans = string.Empty;
+
+            while (string.IsNullOrWhiteSpace(ans))
+            {
+                Console.WriteLine("Palindrome Checker:");
+                ans = Console.ReadLine().ToLower();
+            }
+
+            string reversed = ReverseIt(ans);
+            Console.WriteLine(reversed);
+            Console.WriteLine(ans == reversed ? "Is Palindrome"
+                                : "Not a palindrome");
+
+        }
+        public static string ReverseIt(string ans) { 
+            ans = ans.ToLower();
+            char[] toReverse = ans.ToCharArray();
+            char[] reversed = new char[ans.Length];
+            int counter = 0; 
+            
+            for(int i = reversed.Length-1; i >= 0; i--)
+            {
+               
+                reversed[counter] = toReverse[i];
+                counter++;
+            }
+            
+            return string.Join("" , reversed);
+            
+        }
+    }
+}
